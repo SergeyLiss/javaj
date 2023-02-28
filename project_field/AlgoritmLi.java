1aa005284a80719beba7fa25b6135c9827100119
SergeyR <slog18122018@gmail.com>
2023-02-28 18:09:42 +0300

    обновлен файл AlgoritmLi.java

--- project_field/AlgoritmLi.java
+++ project_field/AlgoritmLi.java
@@ -4,20 +4,26 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class AlgoritmLi {
-    static boolean moveFlag = false;
-    int[][] fieldOut;
-    int[][] pathOut;
+    // private static boolean moveFlag = false;
+    private static int[][] field;
+    private static int[] fieldSize = new int[2]; // Столбцы, строки
+    private static int[][] path;
     
-    public AlgoritmLi(int[] patherIn, int[][] fieldIn) {
-        this.fieldOut = searchAllPath(patherIn, fieldIn);
-        this.pathOut = searchPath(patherIn, this.fieldOut);
+    public AlgoritmLi(int[] size) {
+        fieldSize[0] = size[0];
+        fieldSize[1] = size[1];
+        field = new int[fieldSize[0]][fieldSize[1]];
+        path = new int[fieldSize[0]][fieldSize[1]];
     }
 
-    public static int[][] searchPath(int[] p, int[][] f) {
-        int sizeColumn = f.length;
-        int sizeRow = f[0].length;
-        int[][] result = new int[sizeColumn][sizeRow];
+    // Вывод поля
+    public int[][] getMapLi () { return field;}
+    // Вывод размера поля
+    public int[] getMapSize () { return fieldSize;}
+    // Вывод пути
+    public int[][] getPath () { return path;}
 
+    public void searchPath(int[] p) {
         Queue<Integer> q = new LinkedList<Integer>();
         q.add(p[2]);
         q.add(p[3]);
@@ -29,17 +35,17 @@ public class AlgoritmLi {
             int x = q.remove();
             int y = q.remove();
             sizeQ --;
-            int z = f[x][y] - 1;
+            int z = field[x][y] - 1;
 
             int iMin = x > 0 ? (x-1) : x;
             int jMin = y > 0 ? (y-1) : y;
-            int iMax = x < (sizeColumn - 1) ? (x+2) : (x+1);
-            int jMax = y < (sizeRow - 1) ? (y+2) : (y+1);
+            int iMax = x < (fieldSize[0] - 1) ? (x+2) : (x+1);
+            int jMax = y < (fieldSize[1] - 1) ? (y+2) : (y+1);
 
             for (int i = iMin; i < iMax; i ++){
                 for (int j = jMin; j < jMax; j ++) {
-                    if ((f[i][j] == z) && (z >= 0) && flag) {
-                        result[i][j] = 1;
+                    if ((field[i][j] == z) && (z >= 0) && flag) {
+                        path[i][j] = 1;
                         q.add(i);
                         q.add(j);
                         sizeQ ++;
@@ -48,14 +54,9 @@ public class AlgoritmLi {
                 }
             }
         }
-        return result;
     }
 
-    public static int[][] searchAllPath(int[] p, int[][] f) {
-        int sizeColumn = f.length;
-        int sizeRow = f[0].length;
-        int[][] result = new int[sizeColumn][sizeRow];
-
+    public void algoritmWave(int[] p, int[][] f) {
         Queue<Integer> q = new LinkedList<Integer>();
         f[p[0]][p[1]] ++;
         f[p[2]][p[3]] = 0;
@@ -68,29 +69,28 @@ public class AlgoritmLi {
             int x = q.remove();
             int y = q.remove();
             sizeQ --;
-            int z = result[x][y] + 1;
+            int z = field[x][y] + 1;
 
             int iMin = x > 0 ? (x-1) : x;
             int jMin = y > 0 ? (y-1) : y;
-            int iMax = x < (sizeColumn - 1) ? (x+2) : (x+1);
-            int jMax = y < (sizeRow - 1) ? (y+2) : (y+1);
+            int iMax = x < (fieldSize[0] - 1) ? (x+2) : (x+1);
+            int jMax = y < (fieldSize[1] - 1) ? (y+2) : (y+1);
 
             for (int i = iMin; i < iMax; i ++){
                 for (int j = jMin; j < jMax; j ++) {
-                    if ((f[i][j] == 0) && (result[i][j] == 0)) {
-                        result[i][j] = z;
+                    if ((f[i][j] == 0) && (field[i][j] == 0)) {
+                        field[i][j] = z;
                         q.add(i);
                         q.add(j);
                         sizeQ ++;
                     }
                     else {
                         if (f[i][j] == -1) {
-                            result[i][j] = -1;
+                            field[i][j] = -1;
                         }
                     }
                 }
             }
         }
-        return result;
     }
 }
