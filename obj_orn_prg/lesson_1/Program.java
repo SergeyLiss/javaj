4a6c80d1cfe1e4078ae39d5f0df92395237dc296
SergeyR <slog18122018@gmail.com>
2023-03-23 00:42:23 +0300

    add oop/lesson_1

--- obj_orn_prg/lesson_1/Program.java
+++ obj_orn_prg/lesson_1/Program.java
@@ -2,12 +2,25 @@ package javaj.obj_orn_prg.lesson_1;
 
 public class Program {
     public static void main(String[] args) {
-        // Product cola = new Product("Coca-Cola");
         Lemonade cola = new Lemonade("Coca-Cola");
         cola.setPrice(100);
         cola.setAmount(1);
         cola.setUnit("шт.");
         cola.setVolume(1);
         System.out.println(cola.toString());
+
+        System.out.println();
+
+        Diapers pamp = new Diapers("Pampers");
+        pamp.setPrice(500);
+        pamp.setAmount(20);
+        pamp.setUnit("шт.");
+        pamp.setMinAge(0);
+        pamp.setHypoallergenic(true);
+        pamp.setSize(24);
+        pamp.setMinWeight(1);
+        pamp.setMaxWeight(15);
+        pamp.setType("Впитывающие");
+        System.out.println(pamp.toString());
     }
 }
