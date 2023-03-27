a2341adf3b52c918d1bace184eb029b409f5521f
SergeyR <slog18122018@gmail.com>
2023-03-28 00:39:50 +0300

    add oop/lesson_2

--- obj_orn_prg/lesson_2/Program.java
+++ obj_orn_prg/lesson_2/Program.java
@@ -1,5 +1,25 @@
 package javaj.obj_orn_prg.lesson_2;
 
+import java.util.Random;
+
 public class Program {
-    
+    static Zoopark ten = new Zoopark();
+    static Random rand = new Random();
+
+    public static void main() {
+        // Кот, Тигр, Собака, Волк, Курица, Аист
+        for(int i = 0; i < rand.nextInt(20); i ++) {
+            int j = rand.nextInt(0, 5);
+            if(j == 0) { ten.added(new Stork());}
+            else if(j == 1) { ten.added(new Tiger());}
+            else if(j == 2) { ten.added(new Dog());}
+            else if(j == 3) { ten.added(new Wolf());}
+            else if(j == 4) { ten.added(new Chicken());}
+            else { ten.added(new Cat());}
+        }
+
+        for(int i = 0; i < ten.getSize(); i ++) {
+            System.out.println(ten.getValue(i));
+        }
+    }
 }
--- obj_orn_prg/lesson_2/Zoopark.java
+++ obj_orn_prg/lesson_2/Zoopark.java
@@ -1,5 +1,26 @@
 package javaj.obj_orn_prg.lesson_2;
 
+import java.util.ArrayList;
+
 public class Zoopark {
-    
+    ArrayList<Object> zoop = new ArrayList<>();
+
+    public void added(Object t) { zoop.add(t);}
+
+    public Object getValue(int index) { return zoop.get(index);}
+
+    public int getSize() { return zoop.size();}
+
+    public void delete(int index) {
+        int size = zoop.size();
+        if((index >= size) || (index < 0)) { System.err.println("Ошибка");}
+        else {
+            ArrayList<Object> temp = new ArrayList<>();
+            for(int i = 0; i < size; i ++)
+                if(i != index)
+                    temp.add(zoop.get(i));
+            
+            zoop = temp;
+        }
+    }
 }
