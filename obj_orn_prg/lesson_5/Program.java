531598a923ba1e61514dffc105a860b7182d2ed9
SergeyR <slog18122018@gmail.com>
2023-04-06 22:42:20 +0300

    add oop/lesson_5

--- obj_orn_prg/lesson_5/Program.java
+++ obj_orn_prg/lesson_5/Program.java
@@ -2,5 +2,14 @@
 
 class Program {
 
+    public static void main(String[] args) {
+        String alf = "((4+84)7)-3*(9(64+7))";
+
+        Calculate s = new Calculate(alf);
+        s.calculation();
+        
+        System.out.println(s.expression);
+    }
+
     
 }
